f761eec7779fdd2dc5a7aa718353b2944e8fedb7
Chase Olsen <skalolsen@gmail.com>
2024-03-21 19:48:22 -0600

    specializing style

--- neslo-frontend/src/app/home/home.component.ts
+++ neslo-frontend/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+import { Component, HostListener, Renderer2, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +9,7 @@ import { Component, HostListener, ElementRef, ViewChild, Renderer2 } from '@angu
 })
 export class HomeComponent {
 
-  @ViewChild('heroSection', { static: false }) heroSection: ElementRef;
-  divEl = viewChild<ElementRef>('el'); 
+  @ViewChild('vikingImage', { static: false }) vikingImage!: ElementRef<HTMLImageElement>;
   constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
@@ -19,13 +18,13 @@ export class HomeComponent {
 
   @HostListener('window:scroll', ['$event']) 
   handleScroll(): void {
-    const heroSection = this.renderer.selectRootElement('.hero-section', true);
     const scrollPosition = window.scrollY;
     const height = window.innerHeight;
-    const opacity = 1 - (scrollPosition / height);
+    // Adjust the blur value based on the scroll position. You can tweak this formula to change how quickly the blur effect increases.
+    const blurValue = Math.min(20, (scrollPosition / height) * 20); // Cap the blur at a maximum value, e.g., 20px
 
-    if (this.heroSection && this.heroSection.nativeElement) {
-      this.heroSection.nativeElement.style.opacity = opacity > 0 ? opacity : 0;
-    }
+    // Use renderer to adjust the CSS filter property for the blur effect
+    this.renderer.setStyle(this.vikingImage.nativeElement, 'filter', `blur(${blurValue}px)`);
+  
   }
 }
