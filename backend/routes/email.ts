1884d548097b63663421bbf86dea74c3c2195271
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-04-09 15:17:43 -0600

    excitiiinnngggg

--- backend/routes/email.ts
+++ backend/routes/email.ts
@@ -1,25 +1,25 @@
 import { Router, Request, Response } from 'express';
 import multer from 'multer';
-import sgMail from '@sendgrid/mail';
+// import sgMail from '@sendgrid/mail';
 const router = Router();
 
-const sendGridApiKey = process.env['SENDGRID_API_KEY'];
-if (!sendGridApiKey) {
-  throw new Error('SENDGRID is not defined');
-}
+// const sendGridApiKey = process.env['SENDGRID_API_KEY'];
+// if (!sendGridApiKey) {
+//   throw new Error('SENDGRID is not defined');
+// }
 
-sgMail.setApiKey(sendGridApiKey);
-const storage  = multer.diskStorage({   
-    filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase();
-    // const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name );
-        // + '-' + Date.now() + '.' + ext);
-    }
-});
-const upload = multer({storage: storage})
-
-router.post('/email', upload.single('file'), async (req: Request, res: Response) => {
+// sgMail.setApiKey(sendGridApiKey);
+// const storage  = multer.diskStorage({   
+//     filename: (req, file, cb) => {
+//     const name = file.originalname.toLowerCase();
+//     // const ext = MIME_TYPE_MAP[file.mimetype];
+//     cb(null, name );
+//         // + '-' + Date.now() + '.' + ext);
+//     }
+// });
+// const upload = multer({storage: storage})
+// upload.single('file'),
+router.post('/email',  async (req: Request, res: Response) => {
     const { text, fromName, fromEmail } = req.body;
     // preserve white spaces
     const htmlText = text.replace(/\n/g, '<br>');
@@ -40,14 +40,14 @@ router.post('/email', upload.single('file'), async (req: Request, res: Response)
     };
   
     // Send mail
-    try {
-      const info = await sgMail.send(msg);
-      console.log('Message sents', info);
-      res.send('Email sent successfully');
-    } catch (error) {
-      console.error('Failed to send email', error);
-      res.status(500).send('Failed to send email');
-    }
+    // try {
+    //   const info = await sgMail.send(msg);
+    //   console.log('Message sents', info);
+    //   res.send('Email sent successfully');
+    // } catch (error) {
+    //   console.error('Failed to send email', error);
+    //   res.status(500).send('Failed to send email');
+    // }
   });
 
 export default router;
--- server.ts
+++ server.ts
@@ -11,78 +11,74 @@ import pdfRoute from './backend/routes/pdf';
 import rateLimit from 'express-rate-limit';
 import cors from 'cors';
 import * as path from 'path';
+import compression from 'compression';
 console.log('app on 321')
 
-// Rate limiting middleware
-const apiLimiter = rateLimit({
-    windowMs: 30 * 60 * 1000, // 30 minutes
-    max: 3, // limit each IP to 3 requests per windowMs
-    handler: (req: Request, res: Response, next) => {
-      res.status(429).json({
-          error: 'Please don"t spam emails, try again after 30 minutes'
-      });
-  }
-  });
- // The Express app is exported so that it can be used by serverless Functions.
+// // Rate limiting middleware
+// const apiLimiter = rateLimit({
+//     windowMs: 30 * 60 * 1000, // 30 minutes
+//     max: 3, // limit each IP to 3 requests per windowMs
+//     handler: (req: Request, res: Response, next) => {
+//       res.status(429).json({
+//           error: 'Please don"t spam emails, try again after 30 minutes'
+//       });
+//   }
+//   });
+//  // The Express app is exported so that it can be used by serverless Functions.
   async function createExpressApp(): Promise<express.Express> {
     const app = express();
-console.log('app on')
-    const __dirname = path.dirname(fileURLToPath(import.meta.url));
-    const browserDistFolder = join(__dirname, '..', 'browser'); 
-    const indexHtml = join(browserDistFolder, 'index.html'); 
+     // Middleware
+     app.use(cors());
+     app.use(compression());
+     app.use(express.json());
+     app.use(express.urlencoded({ extended: false }));
+     
+// console.log('app on')
+//     const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//     const browserDistFolder = join(__dirname, '..', 'browser'); 
+//     const indexHtml = join(browserDistFolder, 'index.html'); 
     
-console.log('d', __dirname)
-    const __filename = fileURLToPath(import.meta.url);
-    const serverDistFolder = __filename;
-    // const browserDistFolder = resolve(serverDistFolder, '../browser');
-    // const indexHtml = join(browserDistFolder, 'index.server.html');
+// console.log('d', __dirname)
+//     const __filename = fileURLToPath(import.meta.url);
+//     const serverDistFolder = __filename;
+//     // const browserDistFolder = resolve(serverDistFolder, '../browser');
+//     // const indexHtml = join(browserDistFolder, 'index.server.html');
   
-    const compression = (await import('compression')).default;
-    app.use(cors());
-    app.use(compression());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    
-// API Routes
-    app.use("/api/email", apiLimiter, emailRoute);
-    app.use("/api/pdf", pdfRoute);
+//
+//     app.use("/api/email", apiLimiter, emailRoute);
+//     app.use("/api/pdf", pdfRoute);
 
-    // Serve static files
-    app.get('*.*', express.static(browserDistFolder, {
-        maxAge: '1y'
-    }));
+//     // Serve static files
+//     app.get('*.*', express.static(browserDistFolder, {
+//         maxAge: '1y'
+//     }));
   
-    // All regular routes use the Angular engine
-    app.get('*', (req: Request, res: Response) => {
-        const { protocol, originalUrl, baseUrl, headers } = req;
-        const commonEngine = new CommonEngine();
-        commonEngine.render({
-            bootstrap,
-            documentFilePath: indexHtml,
-            url: `${protocol}://${headers.host}${originalUrl}`,
-            publicPath: browserDistFolder,
-            providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-        })
-        .then(html => res.send(html))
-        .catch(err => {
-            console.error('Error occurred in server side rendering:', err);
-            res.status(500).send('Server error');
-        });
-    });
-  
-    return app;
+//     // All regular routes use the Angular engine
+//     app.get('*', (req: Request, res: Response) => {
+//         console.log('dick')
+//         const { protocol, originalUrl, baseUrl, headers } = req;
+//         const commonEngine = new CommonEngine();
+//         commonEngine.render({
+//             bootstrap,
+//             documentFilePath: indexHtml,
+//             url: `${protocol}://${headers.host}${originalUrl}`,
+//             publicPath: browserDistFolder,
+//             providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+//         })
+//         .then(html => res.send(html))
+//         .catch(err => {
+//             console.error('Error occurred in server side rendering:', err);
+//             res.status(500).send('Server error');
+//         });
+//     });
+return app;
   }
-  
-
-// Run server
-async function run() {
-    console.log('hello')
-  const port = process.env['PORT'] || 4200;
-  const app = createExpressApp();
-
-  (await app).listen(port, () => {
-      console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+async function startServer() {
+    const app = await createExpressApp();
+    const port = process.env['PORT'] || 4200;
+    app.listen(port, () => {
+        console.log(`Node Express server listening on http://localhost:${port}`);
+    });
 }
 
-run();
+startServer();
