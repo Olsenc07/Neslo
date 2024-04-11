854e9fda5fc643b5c55c6ad2e79b38fd5684150d
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-04-11 16:13:53 -0600

    domain activated

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://neslo.ca/api'
-  apiUrl: 'http://localhost:4200/api'
+  apiUrl: 'https://www.neslo.ca/api'
+  // apiUrl: 'http://localhost:4200/api'
 
 };
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
  export const  config: ApplicationConfig = {
-  providers: [provideServerRendering()]
+  providers: [provideServerRendering(),
+    provideHttpClient(withFetch())
+  ]
 };
 
 export const serverConfig = mergeApplicationConfig(appConfig, config);
