81a0c5187b9cd4d2f47e722625b384f28b800aad
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-05-02 23:56:30 -0600

    const

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://www.neslo.ca/api',
+  apiUrl: 'https://www.neslo.ca/',
   cssUrl: 'https://www.neslo.ca/assets/pdf'
   // apiUrl: 'http://localhost:4200/api',
   // cssUrl: 'http://localhost:4200/assets/pdf'
