654b94340eb66cc42a6bfc3d33d27b78327614dc
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-05-02 23:33:34 -0600

    beauty

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4200/api',
-  cssUrl: 'http://localhost:4200/assets/pdf'
+  apiUrl: 'https://www.neslo.ca/api',
+  cssUrl: 'https://www.neslo.ca/assets/pdf'
+  // apiUrl: 'http://localhost:4200/api',
+  // cssUrl: 'http://localhost:4200/assets/pdf'
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule, TitleStrategy } from '@angular/router'
 import { CustomTitleStrategy } from './services/title-strategy.service';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterModule],
+  imports: [RouterModule, MatProgressBarModule],
   providers: [{ provide: TitleStrategy, useClass: CustomTitleStrategy }]
 })
 export class AppComponent {
