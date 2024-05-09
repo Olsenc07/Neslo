4d5632706edcc0757e7d461a8eaef349b620efda
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-05-08 23:19:55 -0600

    adjust env file

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // apiUrl: 'http://localhost:4200/api',
   // cssUrl: 'http://localhost:4200/assets/pdf'
-  apiUrl: 'https://neslo.ca/api',
-  cssUrl: 'https://neslo.ca/assets/pdf'
+  apiUrl: 'https://www.neslo.ca/api',
+  cssUrl: 'https://www.neslo.ca/assets/pdf'
 };
