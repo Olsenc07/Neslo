9095bbe1609b8fb1e53cfecd62927a45e2eba720
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-05-10 15:05:11 -0600

    proper routes

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
   recaptchaSiteKey: '6Ld7EdcpAAAAAP5b51ypqU3cFb5fvgQ7JxfMbGBf',
-  // apiUrl: 'http://localhost:4200/api',
-  // cssUrl: 'http://localhost:4200/assets/pdf'
   apiUrl: 'https://www.neslo.ca/api',
   cssUrl: 'https://www.neslo.ca/assets/pdf'
 };
