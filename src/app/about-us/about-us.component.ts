53bfc0081c08ecbf9dd28122987039d7c8c28eb4
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-05-12 22:54:41 -0600

    fade ins

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
 import { NgStyle } from '@angular/common';
-import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-about-us',
@@ -11,7 +11,6 @@ import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
-  faInstagramSquare = faInstagramSquare;
   @Input() title!: string;
   @Input() customColor!: boolean;
   @Input() img!: {img: string, alt: string };
